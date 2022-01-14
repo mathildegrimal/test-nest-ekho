352828bd253ca8bc78a97f5842daa67d0bf28325
Mathilde <mat.Grimal@gmail.com>
2022-01-14 14:31:11 +0100

    added dist folder to git

---
+++ dist/users/users.service.d.ts
@@ -0,0 +1,11 @@
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+export declare class UsersService {
+    private usersRepository;
+    constructor(usersRepository: Repository<User>);
+    create(createUserDto: CreateUserDto): Promise<User>;
+    findAll(): Promise<User[]>;
+    findOne(id: number): Promise<User>;
+    remove(id: number): Promise<void>;
+}
